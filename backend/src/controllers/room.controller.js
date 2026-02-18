2de75c993a75b3e768f20249229a22958eee4489
arthurliuxh-gg <arthurliu.xh@gmail.com>
2026-02-18 18:29:52 +0800

    feat: initial project setup (Sprint 0) - complete Hotel PMS development environment

---
+++ backend/src/controllers/room.controller.js
@@ -0,0 +1,25 @@
+const db = require('../config/database');
+
+exports.getRooms = async (req, res) => {
+  res.status(501).json({ error: { message: 'Not implemented yet' } });
+};
+
+exports.getAvailableRooms = async (req, res) => {
+  res.status(501).json({ error: { message: 'Not implemented yet' } });
+};
+
+exports.getRoomById = async (req, res) => {
+  res.status(501).json({ error: { message: 'Not implemented yet' } });
+};
+
+exports.createRoom = async (req, res) => {
+  res.status(501).json({ error: { message: 'Not implemented yet' } });
+};
+
+exports.updateRoom = async (req, res) => {
+  res.status(501).json({ error: { message: 'Not implemented yet' } });
+};
+
+exports.deleteRoom = async (req, res) => {
+  res.status(501).json({ error: { message: 'Not implemented yet' } });
+};
